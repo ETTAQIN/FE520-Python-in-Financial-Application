908e2e1f884a8fbf18456487009c922fd5b4a72c
MiMao Da <35322256+YanWu1230@users.noreply.github.com>
2019-05-01 14:41:06 -0400

    fetchdata

--- Final_Project/scrap.py
+++ Final_Project/scrap.py
@@ -1,37 +1,55 @@
 import tweepy
-import numpy as np
 import pandas as pd
+import numpy as np
+from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+from credentials import *
 
-
+# API's setup:
+# Twitter App access keys for @user
 consumer_key = 'MiOGqZIoA779scx9DP7pSeYzM'
 consumer_secret = 'e9IurOqeoTPwZyLoChvQXCTo363CYt76Ijjy2EQjtT1F4GiznG'
 access_token = '1118579811402825733-7P03ASm89v3EchFleWogbhvcl2U9Wr'
 access_token_secret = '6uIksmLCHb1CxeAIBW75hT4I7DQ6jjUGmQkujuR2Wf2Xi'
 
-# API's setup
+# get the manipulate right
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# use the user_timeline function to get tweets
+last_id = ''
+tweets_summary = []
+while True:
+    try:
+        if last_id == '':
+            tweets = api.user_timeline(screen_name='realDonaldTrump', count=200)
+            last_id = tweets[-1].id
+        else:
+            tweets = api.user_timeline(screen_name='realDonaldTrump', count=200, max_id=last_id)
+            last_id = tweets[-1].id
+
+        tweets_summary.extend(tweets)
+
+    except Exception:
+        print("finished")
+        break
 
 
-def twitter_setup():
-    """
-    Utility function to setup the Twitter's API
-    :return: API with authentication
-    """
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    return api
+print("Number of tweets extracted:{}.\n".format(len(tweets_summary)))
+# check the things we get
+# for tweet in tweets_summary:
+#     print(tweet.text)
 
-
-public_tweets = twitter_setup().user_timeline('realDonaldTrump', count=200)
-
-data = pd.DataFrame(data=[tweet.text for tweet in public_tweets], columns=['Tweets'])
-data['length'] = np.array([len(tweet.text) for tweet in public_tweets])
-data['Date'] = np.array([tweet.created_at for tweet in public_tweets])
-data['Source'] = np.array([tweet.source for tweet in public_tweets])
-data['Likes'] = np.array([tweet.favorite_count for tweet in public_tweets])
-data['RTs'] = np.array([tweet.retweet_count for tweet in public_tweets])
-
-print(data.head())
+# create a pandas dataframe as follows:
+data = pd.DataFrame(data=[tweet.text for tweet in  tweets_summary], columns=['Tweets'])
+data['length'] = np.array([len(tweet.text) for tweet in tweets_summary])
+data['ID'] = np.array([tweet.id for tweet in tweets_summary])
+data['Date'] = np.array([tweet.created_at for tweet in tweets_summary])
+data['Source'] = np.array([tweet.source for tweet in tweets_summary])
+data['Likes'] = np.array([tweet.favorite_count for tweet in tweets_summary])
+data['RTs'] = np.array([tweet.retweet_count for tweet in tweets_summary])
 
 data.to_csv('tweets.csv')
 
